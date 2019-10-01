d120fbdc6935cc18fd1fd868d95e5e44f7b0c31c
thegleb <thegleb@users.noreply.github.com>
2019-10-01 00:08:32 -0700

    Remove unneeded files, add .gitignore

--- Path_Planning.build/Debug/path_planning.build/Objects-normal/x86_64/vehicle.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/gleb/dev/Udacity-Self-Driving-Car-Engineer/CarND-Path-Planning-Project/src/vehicle.cpp \
-  /Users/gleb/dev/Udacity-Self-Driving-Car-Engineer/CarND-Path-Planning-Project/src/vehicle.hpp
